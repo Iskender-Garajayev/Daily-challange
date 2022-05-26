38f89d5d2f37c08bc94c12abbb10e02d6d11b555
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-05-26 22:15:09 +0300

    react project

---
+++ gergedan-web/src/App.js
@@ -0,0 +1,14 @@
+import Header from './Head/Header';
+import Footer from './Foot/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ gergedan-web/src/Head/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <div>
+          <img className='header-logo' src='https://cdn.pixabay.com/photo/2017/09/19/13/54/animal-2765319_960_720.png' alt='Gergedan Logo' />
+        </div>
+        <div className='nav-bar'>
+            <a href='#'>home</a>
+            <a href='#'>menu</a>
+            <a href='#'>categori</a>
+            <a href='#'>about us</a>
+            <a href='#'>contact</a>
+        </div>
+      </div>
+    )
+  }
+}
