7f8a8bbfb07bd4d0c07092b174e5d1b2fe2a51ba
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-02 21:15:50 +0300

    constructor update

--- constructor/src/Main.js
+++ constructor/src/Main.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Main extends Component {
+  // create by "rco"
+  constructor(props) {
+    super(props)
+    console.log("Constructor Runned")
+    console.log("Props=>", this.props)
+  }
   render() {
     return (
       <div>Main</div>
