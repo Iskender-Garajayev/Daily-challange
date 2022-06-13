81872441468e0f1a41c960cc1a894646416274cc
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-14 02:39:40 +0300

    scu

--- constructor/src/ChildeMain.js
+++ constructor/src/ChildeMain.js
@@ -18,8 +18,20 @@ export default class ChildeMain extends Component {
   // Created By "SCU"
   shouldComponentUpdate(nextProps, nextState) { 
     console.log("shouldComponentUpdate Runned")
+    if (nextState.number>30) {
+    return true;
+    } else {
     return false;
+    }
    }
+
+  //  Created By CWUP
+   componentWillUpdate(nextProps, nextState){
+    console.log("ComponentWillUpdate Runned")
+   }
+
+   state={number:0}
+   changeNumber=(event)=>{this.setState({number:event.target.value})}
   render() {
     console.log("Child Render Runned")
 
@@ -27,6 +39,8 @@ export default class ChildeMain extends Component {
       <div>
         ChildeMain
         <h2>Props: {this.props.name}</h2>
+        <input name='name' id='name' onChange={this.changeNumber}></input>
+        <br/>Number: {this.state.number} /10= {this.state.number/10}
       </div>
     )
   }
