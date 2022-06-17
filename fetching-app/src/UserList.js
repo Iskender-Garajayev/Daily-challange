60147b9461ce46d93e4c816ae967bdc074aebb4f
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-17 04:26:07 +0300

    fetching resp

--- fetching-app/src/UserList.js
+++ fetching-app/src/UserList.js
@@ -5,12 +5,15 @@ export default class UserList extends Component {
 
   // Created By "cdm"
   componentDidMount() { 
-    
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(jsonData => this.setState({users:jsonData}))
    }
   render() {
     return (
       <div>
-        
+        <h1>Users</h1>
+        {this.state.users.map((item)=>(<div key={item.id}>{item.name}</div>))}
       </div>
     )
   }
