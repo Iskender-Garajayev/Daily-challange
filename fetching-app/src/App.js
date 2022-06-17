4ec35b814a1782fff7c04d9f7f286ed35d2174aa
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-17 23:47:19 +0300

    Router

--- fetching-app/src/App.js
+++ fetching-app/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import UserList from './UserList';
+import {BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      App Component
-      <UserList/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        App Component
+        <UserList/>
+      </div>
+    </BrowserRouter>
   );
 }
 
