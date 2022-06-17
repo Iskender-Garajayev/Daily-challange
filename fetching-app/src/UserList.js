9c48b5564eb598edff8275ee0cc670a5eaf5a0b2
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-17 03:59:27 +0300

    fetching

---
+++ fetching-app/src/UserList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class UserList extends Component {
+  state={users:[]}
+
+  // Created By "cdm"
+  componentDidMount() { 
+    
+   }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
