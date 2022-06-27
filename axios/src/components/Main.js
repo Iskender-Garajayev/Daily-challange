79c39fec6cd5b5d2ad038e659e0edc0735d8559e
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-27 05:25:12 +0300

    main

--- axios/src/components/Main.js
+++ axios/src/components/Main.js
@@ -5,7 +5,7 @@ import pp1 from '../img/pp1.png'
 function Main() {
   return (
     <div>
-      <div>
+      <div className='main_img'>
         <img className='pp1' src={pp1} alt="" />
       </div>
     </div>
