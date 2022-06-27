40cf44718765423389e15f0159b45209a4f18319
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-27 05:20:18 +0300

    rename img

--- axios/src/components/Main.js
+++ axios/src/components/Main.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Main.css'
+import pp1 from '../img/pp1.png'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div>
+      <div>
+        <img className='pp1' src={pp1} alt="" />
+      </div>
+    </div>
   )
 }
 
