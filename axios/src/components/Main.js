be222d5fb5e55b1f8cdf30b42bef0ffe940d12c3
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-27 07:54:48 +0300

    fix background

--- axios/src/components/Main.js
+++ axios/src/components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Main.css'
 import pp1 from '../img/pp1.png'
+import bg from '../img/bg.png'
+
 
 function Main() {
   return (
     <div>
       <div className='main_img'>
-        <img className='pp1' src={pp1} alt="" />
+        <img className='pp1' src={bg} alt="" />
       </div>
     </div>
   )
