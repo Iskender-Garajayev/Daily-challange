77a19bc3129dffcf9d3522e723b415fadb08a1dc
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-21 05:06:27 +0300

    update

--- new-project/src/Components/Header/Header.js
+++ new-project/src/Components/Header/Header.js
@@ -3,7 +3,15 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      
+      <ul>
+        <li>Home</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   )
 }
