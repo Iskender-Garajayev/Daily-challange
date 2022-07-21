f1b4d7eaf364a0f2b8a319a2518655631d7ff78d
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-21 04:42:00 +0300

    add Components

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Body from './Components/Body/Body';
 
 function App() {
   return (
     <div className="App">
-  
+      <Header />
+      <Body/>
+      <Footer/>
     </div>
   );
 }
